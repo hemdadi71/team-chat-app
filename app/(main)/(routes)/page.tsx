e62ec92466b9348576ede73a0a2c425ea39110d5
hemdadi71 <hemdadi71@gmail.com>
2023-09-05 20:41:01 +0430

    project config completed

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+
+export default function Home() {
+  return (
+    <>
+      <div className="">this is a protected routes</div>
+    </>
+  )
+}
